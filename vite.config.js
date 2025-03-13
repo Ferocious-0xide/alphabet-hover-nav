02e48bb6412a3657da1009200595fdbe811203a4
Ben Rust <brust@brust-ltm38bh.internal.salesforce.com>
2025-03-13 16:17:42 -0400

    Update Vite config to allow Heroku hostname

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: process.env.PORT || 5173,
+    host: '0.0.0.0',
+    allowedHosts: ['alphabet-hover-nav-a113146cee37.herokuapp.com', '.herokuapp.com']
+  }
 })
